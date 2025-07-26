91f4f4fbe671a0505ccd70ed0f0d012b70cd11f0
sami-g <samuelgirma888@gmail.com>
2025-07-26 21:12:26 +0300

    g

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,33 +1,19 @@
- 
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext_lazy as _
-from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, UserProfile, Author, Book, Library, Librarian
 
-class CustomUserAdmin(BaseUserAdmin):
-    # The forms to add and change user instances — use Django's defaults or custom if needed
+class CustomUserAdmin(UserAdmin):
     model = CustomUser
-
-    # Fields to show in the User list page
-    list_display = ('email', 'full_name', 'is_staff', 'is_superuser')
-    list_filter = ('is_staff', 'is_superuser', 'is_active')
-
-    # Fields grouped for editing a user
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields': ('full_name',)}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Important Dates'), {'fields': ('last_login',)}),
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
 
-    # Fields for creating a new user in admin
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'full_name', 'password1', 'password2', 'is_staff', 'is_active')}
-        ),
-    )
-
-    search_fields = ('email', 'full_name')
-    ordering = ('email',)
-
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(UserProfile)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
