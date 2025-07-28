39cdc474204258ad9aa770ce966185a5ecd11f4b
sami-g <samuelgirma888@gmail.com>
2025-07-28 14:22:13 +0300

    final

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(blank=True, null=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
 
-    class Meta:
-        permissions = [
-            ("can_view", "Can view book"),
-            ("can_create", "Can create book"),
-            ("can_edit", "Can edit book"),
-            ("can_delete", "Can delete book"),
-        ]
+    def __str__(self):
+        return self.username
+    
