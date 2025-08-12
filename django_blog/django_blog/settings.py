92d6b479401fad12f6bd11ce4d4437b35273ab7b
sami-g <samuelgirma888@gmail.com>
2025-08-13 00:42:01 +0300

    Initial Django blog setup: models, templates, static

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,13 +79,18 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'postgres',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
