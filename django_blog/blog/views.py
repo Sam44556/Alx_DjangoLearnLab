65ef84e8cd73424e3c168c15eef50df2e44582cd
sami-g <samuelgirma888@gmail.com>
2025-08-13 00:36:35 +0300

    Initial Django blog setup: models, templates, static

---
+++ django_blog/blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Post
+
+def home(request):
+    posts = Post.objects.order_by('-published_date')[:10]
+    return render(request, 'blog/home.html', {'posts': posts})
