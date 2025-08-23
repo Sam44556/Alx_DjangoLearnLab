a7eacab439b9f5bff65e136d06bfd3867db735ab
sami-g <samuelgirma888@gmail.com>
2025-08-24 00:27:45 +0300

    g

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token   # <-- required import
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)   # <-- required
+    # Explicit password field (required by test)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
@@ -21,7 +22,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.profile_picture = validated_data.get("profile_picture", None)
         user.save()
 
-        # Generate token here
-        Token.objects.create(user=user)   # <-- required
+        # Generate token on registration
+        Token.objects.create(user=user)
 
         return user
