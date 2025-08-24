14acbeea1b01bc707e340de55e7adc070889acaf
sami-g <samuelgirma888@gmail.com>
2025-08-24 22:40:25 +0300

    g

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -9,6 +9,8 @@ router.register(r'users', UserViewSet)
 urlpatterns = router.urls
 
 urlpatterns += [
+    path("follow/<int:user_id>/", views.follow_user, name="follow_user"),
+    path("unfollow/<int:user_id>/", views.unfollow_user, name="unfollow_user"),
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,14 @@
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
-
+from django.urls import path
+from . import views
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
 urlpatterns = router.urls
+
+
+urlpatterns += [
+    path("feed/", views.feed, name="feed"),
+]
